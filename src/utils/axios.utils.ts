310d5a9eb370db32bc56426bcebc3c64fa602e82
abhishek <abhiskg20@gmail.com>
2022-11-24 20:44:59 +0600

    axios interceptor created

---
+++ src/utils/axios.utils.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosResponse, AxiosError } from "axios";
+
+const client = axios.create({ baseURL: "http://localhost:5000" });
+
+export const request = ({ ...options }) => {
+  client.defaults.headers.common.Authorization = `Bearer token`;
+
+  const onSuccess = (response: AxiosResponse) => response;
+  const onError = (error: AxiosError) => {
+    return error;
+  };
+  return client(options).then(onSuccess).catch(onError);
+};
